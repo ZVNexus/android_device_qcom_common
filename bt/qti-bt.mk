99bc6b333c6b84078dcc89823080540e70f6bca3
Alexander Koskovich <zvnexus@outlook.com>
2020-08-25 15:12:50 +0000

    bt: Configure component for built vendor.
    Change-Id: Ic8d241d3d2a4487907e956bbdf9ee3bbdfe8d69b

--- bt/qti-bt.mk
+++ bt/qti-bt.mk
@@ -16,6 +16,13 @@
 include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
 
+# Packages
+PRODUCT_PACKAGES += \
+    libbluetooth_audio_session \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_dun@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
